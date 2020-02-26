fd088be5f8e392492145be048746d71b7597aca7
Per Cederqvist <ceder@lysator.liu.se>
2020-02-26 23:16:34 +0100

    Display a message on the display during an OTA upgrade

--- anavi-miracle-controller-sw/anavi-miracle-controller-sw.ino
+++ anavi-miracle-controller-sw/anavi-miracle-controller-sw.ino
@@ -813,6 +813,7 @@ void do_ota_upgrade(char *text)
         Serial.print(":");
         Serial.print(port);
         Serial.println(file);
+        drawDisplay("Performing", "OTA upgrade.", "Stand by.");
         ESPhttpUpdate.setLedPin(pinAlarm, HIGH);
         WiFiClient update_client;
         t_httpUpdate_return ret = ESPhttpUpdate.update(update_client,
@@ -829,6 +830,7 @@ void do_ota_upgrade(char *text)
 
         case HTTP_UPDATE_OK:
             Serial.println("HTTP_UPDATE_OK");
+            drawDisplay("OTA OK.", "Rebooting.", "");
             break;
         }
     }
